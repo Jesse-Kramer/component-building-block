d0d6dc4033b9d0f98c0b698d17bbecd9b35bd822
Jesse Kramer <jessekramer15@gmail.com>
2024-11-21 13:40:13 +0100

    JK - Fixed build

---
+++ src/routes/home/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+    import Story from '../../lib/components/story.svelte';
+	import Menu from '../../lib/components/menu.svelte';
+</script>
+<section>
+	<Menu />
+	<Story />
+</section>
+
+<style>
+    section{
+        background-color: purple;
+    }
+</style>
